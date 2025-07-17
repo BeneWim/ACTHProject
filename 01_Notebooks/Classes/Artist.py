02a321ceeec9af7852c977da0cc543c2aa331a34
Francesca Chessari <francescaevachessari@gmail.com>
2025-07-17 13:25:08 +0200

    Everything

--- 01_Notebooks/Classes/Artist.py
+++ 01_Notebooks/Classes/Artist.py
@@ -5,77 +5,56 @@ import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON 
 
 class Artist:
-    def __init__(self, display_name, nationality=None, wikidata_uri=None):
-        # Artist Represents a person who created one or more artifacts.
-        # You choose what attributes to store (e.g., name, lifespan, nationality, movement), and how to structure artist-artifact relationships.
-        # You can create subclasses of artists for specific types (i.e., human vs organization)
-
+    def __init__(self, display_name, nationality=None, wikidata_uri=None, date_of_birth=None): #defining the class and its parameters (parameters found in the MET dataset but also through enritchment)
         self.display_name = eval(display_name)[0] if isinstance(display_name, str) else "Unknown" #eval= parses string to python object
         self.nationality = nationality
         self.wikidata_uri = wikidata_uri
-        self.date_of_birth = None
+        self.date_of_birth = date_of_birth
 
     @classmethod
-    def from_dataframe(cls, df: pd.DataFrame, index: int):
-        """
-        Instantiate an Artifact from a row in a pandas DataFrame.
+    def from_dataframe(cls, df: pd.DataFrame, index: int): #creates an artist from one row of the dataframe using the parameters defined above, by their index in the dataframe and the name of the column
 
-        Args:
-            df (pd.DataFrame): DataFrame containing artifact metadata.
-            index (int): Index of the row to convert.
-
-        Returns:
-            Artifact: An instance of the Artifact class.
-        """
         return cls(
             df.loc[index, "Artist Display Name"],
             df.loc[index, "Artist Nationality"],
             df.loc[index, "Artist Wikidata URL"]
         )
 
-    def to_rdf(self):
-        # returns an rdflib.Graph representation of the artist (simplistic, does not have to have all the attributes represented in RDF, just a proof of concept)
+    def to_rdf(self): #creates a rdf-graph for artist 
         g = Graph()
 
-        CRM = Namespace("http://www.cidoc-crm.org/cidoc-crm/") #creating a namespace object
+        CRM = Namespace("http://www.cidoc-crm.org/cidoc-crm/") #defining the ontologies 
         DC = Namespace("http://purl.org/dc/elements/1.1/")
         EX = Namespace("http://w3id.org/example/")
         FOAF = Namespace("http://xmlns.com/foaf/0.1/")
         Schema = Namespace("http://schema.org/")
 
-        g.bind("crm", CRM) # binding it to the graph so when we serialize it, it will use #crm:something instead of the full namespace
+        g.bind("crm", CRM) #binding the abbreviation to the graph so when we serialize it, it will use #crm:something instead of the full namespace
         g.bind("dc", DC)
         g.bind("ex", EX)
 
         if self.wikidata_uri is None: 
-            self.wikidata_uri = f'http://w3id.org/example/artist/{self.display_name.replace(" ", "_")}'
-        artist_uri = URIRef(self.wikidata_uri)
+            self.wikidata_uri = f'http://w3id.org/example/artist/{self.display_name.replace(" ", "_")}' #if there is no wikidata_uri in the dataframe, this line creates one out of the display_name. Spaces have to be replaced by _ to create a valid link.
+        artist_uri = URIRef(self.wikidata_uri) #creates a artist_uri out of the wikidata_uri
 
-        g.add((artist_uri, RDF.type, CRM["E20_Person"]))
+        g.add((artist_uri, RDF.type, CRM["E20_Person"])) #adds triples using the ontologies 
         g.add((artist_uri, FOAF.name, Literal(self.display_name)))
         g.add((artist_uri, Schema.nationality, Literal(self.nationality)))
         
         if self.date_of_birth: 
-            g.add((artist_uri, Schema.birthDate, Literal(self.date_of_birth)))
+            g.add((artist_uri, Schema.birthDate, Literal(self.date_of_birth))) #adds a birth date if there is one 
         
         
         return g
     
-    def print_rdf(self):
-        """
-        Print the RDF serialization of the artifact in Turtle format.
-   def     """
+    def print_rdf(self): #prints the rdf graph in a turtle format
         graph = self.to_rdf()
 
         print(graph.serialize(format="turtle"))
 
-    def wikidata_enrich(self):
-        """
-        Enrich tag information using SPARQL queries to Wikidata.
-
-        Appends English descriptions of tag entities to the `enriched_tags` list.
-        """
-        if not isinstance(self.wikidata_uri, str):
+    def wikidata_enrich(self): #enriches the class information using a SPARQL querie to wikidata 
+  
+        if not isinstance(self.wikidata_uri, str): #skips the enrichment if the data is not a string and prints an answer 
             print("wikidata_uri is not a str, skipping enrichment.")
             return
 
@@ -83,7 +62,7 @@ class Artist:
         sparql.setReturnFormat(JSON)
 
     
-        qid = self.wikidata_uri.split("/")[-1]
+        qid = self.wikidata_uri.split("/")[-1] #queries the optional birth date
         query = f"""
         SELECT ?date_of_birth WHERE {{
             OPTIONAL {{ wd:{qid} wdt:P569 ?date_of_birth . }}
@@ -91,13 +70,12 @@ class Artist:
         }}
         LIMIT 1
         """
-        try:
+        try: #sends the query to wikidata to look if there is any infomation that was asked for
             sparql.setQuery(query)
             results = sparql.query().convert()
-            for result in results["results"]["bindings"]:
+            for result in results["results"]["bindings"]: #if there is information it gets saved 
                 date_of_birth = result.get("date_of_birth", {}).get("value", "")
                 self.date_of_birth = date_of_birth
-        except Exception as e:
-            print(f"Error querying {qid}: {e}")
 
-    
+        except Exception as e:
+            print(f"Error querying {qid}: {e}") #prints a error if the query doesn't work
--- README.md
+++ README.md
@@ -9,7 +9,6 @@ https://openaccess-api.clevelandart.org/
 Project summary: <br>
   Goal of the Project: 
    Answer the question: Has the reception of artifacts at the MET changed over time in terms of their type, artist, culture, size and credit-line? <br>
-  Design Choices: <br>
 
   Division of group work: <br>
     Cleaning the Data - Ben <br>
@@ -25,5 +24,9 @@ Project summary: <br>
       ML results - <br>
       RDF graphs - <br>
 
+We only used the data in the dataframe that was in the department of "Greek and Roman Art". 
 
-Creating the Artist class: 
+
+Developing the Classes explanaition of the functions: 
+  1. defining the class and its parameters in __init__ 
+  2. 
